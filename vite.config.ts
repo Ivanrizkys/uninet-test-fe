b9a24cbb40d7c4852a00e0402c9c43b220ad3eba
Ivan Rizky Saputra <ivnrzkys@gmail.com>
2024-02-09 10:35:52 +0700

    chore: setup tailiwndcss and shadcnui, install react router

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { VitePWA } from "vite-plugin-pwa";
@@ -40,4 +41,9 @@ export default defineConfig({
       },
     }),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
