c412c9a492ce081d32ae5ca532625258ed990f68
Ivan Rizky Saputra <ivnrzkys@gmail.com>
2024-02-09 23:21:43 +0700

    feat: wraping protected route and adding navbar

--- src/components/templates/Protected/index.tsx
+++ src/components/templates/Protected/index.tsx
@@ -1,12 +1,18 @@
 // import { ReactNode } from "react";
 import { Outlet } from "react-router-dom";
+import Navbar from "@/components/organims/Navbar";
 
 // interface ProtectedProps {
 //   children: ReactNode;
 // }
 
 function Protected() {
-  return <Outlet />;
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
 }
 
 export default Protected;
