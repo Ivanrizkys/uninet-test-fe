57707f6997968e79f91e6e3d6ae3b176ad2f1e23
Ivan Rizky Saputra <ivnrzkys@gmail.com>
2024-02-09 14:51:10 +0700

    feat: implement UI register page

--- src/components/pages/Register/index.tsx
+++ src/components/pages/Register/index.tsx
@@ -1,7 +1,27 @@
-function Register () {
+import { useState } from "react";
+import { SubmitHandler } from "react-hook-form";
+import Auth from "@/components/templates/Auth";
+import AuthForm, { AuthFormValues } from "@/components/organims/AuthForm";
+
+function Register() {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const handleRegister: SubmitHandler<AuthFormValues> = (data) => {
+    setIsLoading(true);
+    console.log(data);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  };
+
   return (
-    <div>Register Page</div>
-  )
+    <Auth
+      variant="register"
+      title="Create an Account"
+      description="Please enter your email and password"
+      form={<AuthForm isLoading={isLoading} handleAuth={handleRegister} />}
+    />
+  );
 }
 
 export default Register;
