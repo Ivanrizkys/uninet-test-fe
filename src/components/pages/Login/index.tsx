af2a704ba7b0bb1c29261bb7014811c7b7da5654
Ivan Rizky Saputra <ivnrzkys@gmail.com>
2024-02-09 14:31:37 +0700

    feat: implement ui login page

--- src/components/pages/Login/index.tsx
+++ src/components/pages/Login/index.tsx
@@ -1,7 +1,26 @@
-function Login () {
+import { useState } from "react";
+import Auth from "@/components/templates/Auth";
+import { SubmitHandler } from "react-hook-form";
+import LoginForm, { LoginFormValues } from "@/components/organims/LoginForm";
+
+function Login() {
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  
+  const handleLogin: SubmitHandler<LoginFormValues> = (data) => {
+    setIsLoading(true)
+    console.log(data);
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 3000)
+  };
+
   return (
-    <div>Login page</div>
-  )
+    <Auth
+      title="Login to Dashboard"
+      description="Please enter your email and password"
+      form={<LoginForm isLoading={isLoading} handleLogin={handleLogin}  />}
+    />
+  );
 }
 
 export default Login;
