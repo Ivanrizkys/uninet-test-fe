40b8073261e92277d0518e7343e8322a68c9b0f9
Ivan Rizky Saputra <ivnrzkys@gmail.com>
2024-02-10 14:11:25 +0700

    feat: implement login feature

--- src/components/pages/Login/index.tsx
+++ src/components/pages/Login/index.tsx
@@ -1,17 +1,37 @@
+import Cookies from "js-cookie";
 import { useState } from "react";
+import toast from "react-hot-toast";
+import Toast from "@/components/ui/toast";
+import axiosInstance from "@/config/axios";
 import Auth from "@/components/templates/Auth";
+import { useNavigate } from "react-router-dom";
 import { SubmitHandler } from "react-hook-form";
 import AuthForm, { AuthFormValues } from "@/components/organims/AuthForm";
 
 function Login() {
-  const [isLoading, setIsLoading] = useState<boolean>(false)
-  
-  const handleLogin: SubmitHandler<AuthFormValues> = (data) => {
-    setIsLoading(true)
-    console.log(data);
-    setTimeout(() => {
-      setIsLoading(false)
-    }, 3000)
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const navigate = useNavigate();
+
+  const handleLogin: SubmitHandler<AuthFormValues> = async (data) => {
+    setIsLoading(true);
+    try {
+      const res = await axiosInstance.post("/login", {
+        email: data.email,
+        password: data.password,
+      });
+      Cookies.set("token", res.data.token as string);
+      toast.custom(() => (
+        <Toast variant="success" message="Login successfully!" />
+      ));
+      setIsLoading(false);
+      navigate("/");
+    } catch (err) {
+      setIsLoading(false);
+      toast.custom(() => (
+        <Toast variant="error" message="Login failed, please try again!" />
+      ));
+    }
   };
 
   return (
@@ -19,7 +39,7 @@ function Login() {
       variant="login"
       title="Login to Dashboard"
       description="Please enter your email and password"
-      form={<AuthForm isLoading={isLoading} handleAuth={handleLogin}  />}
+      form={<AuthForm isLoading={isLoading} handleAuth={handleLogin} />}
     />
   );
 }
