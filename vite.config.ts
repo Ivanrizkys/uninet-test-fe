e31e6b4610620b64cb77820f96a8f601f857861f
Ivan Rizky Saputra <ivnrzkys@gmail.com>
2024-02-11 01:33:02 +0700

    feat: change name pwa app

--- vite.config.ts
+++ vite.config.ts
@@ -11,8 +11,8 @@ export default defineConfig({
       registerType: "autoUpdate",
       includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
       manifest: {
-        name: "Vite PWA Project",
-        short_name: "Vite PWA Project",
+        name: "User Management Dashboard",
+        short_name: "User Management Dashboard",
         theme_color: "#ffffff",
         icons: [
           {
