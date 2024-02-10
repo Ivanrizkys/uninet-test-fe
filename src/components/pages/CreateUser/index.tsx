e714d93f444b2bb345c1998348f127ac0df2ec84
Ivan Rizky Saputra <ivnrzkys@gmail.com>
2024-02-10 11:12:56 +0700

    feat: implement feature create user

--- src/components/pages/CreateUser/index.tsx
+++ src/components/pages/CreateUser/index.tsx
@@ -1,20 +1,39 @@
 import { useState } from "react";
+import toast from "react-hot-toast";
+import { v4 as uuidv4 } from "uuid";
+import { db } from "@/config/firebase";
+import Toast from "@/components/ui/toast";
+import { useNavigate } from "react-router-dom";
 import { SubmitHandler } from "react-hook-form";
 import HeaderApp from "@/components/molecules/HeaderApp";
+import { Timestamp, addDoc, collection } from "firebase/firestore";
 import UserForm, { UserFormValues } from "@/components/organims/UserForm";
 import UserManagementTemplate from "@/components/templates/UserManagement";
 
 function CreateUser() {
-  const [isLoading, setIsLoading] = useState<boolean>(false)
-  
-  const handleCreateUser: SubmitHandler<UserFormValues> = (data) => {
-    setIsLoading(true)
-    console.log(data)
-    setTimeout(() => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const navigate = useNavigate()
+
+  const handleCreateUser: SubmitHandler<UserFormValues> = async (data) => {
+    setIsLoading(true);
+    try {
+      await addDoc(collection(db, "users"), {
+        id: uuidv4(),
+        name: data.name,
+        email: data.email,
+        username: data.username,
+        createdAt: Timestamp.now(),
+      });
       setIsLoading(false)
-    }, 1000)
-  }
-  
+      toast.custom(() => <Toast variant="success" message="Success create new user!" />);
+      navigate("/")
+    } catch (err) {
+      setIsLoading(false)
+      toast.custom(() => <Toast variant="error" message="Error when create new user!" />);
+    }
+  };
+
   return (
     <UserManagementTemplate
       header={
@@ -23,10 +42,7 @@ function CreateUser() {
           description="Insert name, username, and email for create new user"
         />
       }
-      form={<UserForm 
-        isLoading={isLoading}
-        handleUser={handleCreateUser}
-      />}
+      form={<UserForm isLoading={isLoading} handleUser={handleCreateUser} />}
     />
   );
 }
