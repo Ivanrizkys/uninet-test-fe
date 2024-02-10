c8f86cde27ac23c5012c81e6dfd05e2bb1bf50ee
Ivan Rizky Saputra <ivnrzkys@gmail.com>
2024-02-10 14:14:44 +0700

    feat: implement edit user feature

--- src/components/pages/EditUser/index.tsx
+++ src/components/pages/EditUser/index.tsx
@@ -1,22 +1,84 @@
-import { useState } from "react";
+import toast from "react-hot-toast";
+import { User } from "@/types/users";
+import { db } from "@/config/firebase";
+import Toast from "@/components/ui/toast";
+import { useEffect, useState } from "react";
 import { SubmitHandler } from "react-hook-form";
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import HeaderApp from "@/components/molecules/HeaderApp";
 import UserForm, { UserFormValues } from "@/components/organims/UserForm";
 import UserManagementTemplate from "@/components/templates/UserManagement";
+import { collection, query, where, getDocs, updateDoc, doc } from "firebase/firestore";
+
+const dataCollectionRef = collection(db, "users");
 
 function EditUser() {
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [loadingEdit, setLoadingEdit] = useState<boolean>(false);
+  const [loadingGet, setLoadingGet] = useState<boolean>(true);
+  const [user, setUser] = useState<Omit<User, "createdAt">>({
+    id: "",
+    name: "",
+    email: "",
+    username: "",
+  });
 
-  const [searchParams] = useSearchParams();
+  const params = useParams();
+  const navigate = useNavigate();
 
-  const handleEditUser: SubmitHandler<UserFormValues> = (data) => {
-    setIsLoading(true);
-    console.log(data);
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 1000);
+  useEffect(() => {
+    (async function () {
+      if (params.id) {
+        setLoadingGet(true);
+        const q = query(dataCollectionRef, where("id", "==", params.id));
+        const querySnapshot = await getDocs(q);
+        if (querySnapshot.empty) {
+          toast.custom(() => (
+            <Toast variant="error" message="User does't exist" />
+          ));
+          setLoadingGet(false);
+          return;
+        }
+        querySnapshot.forEach((doc) => {
+          if (doc.exists()) {
+            setUser({
+              id: doc.data().id as string,
+              name: doc.data().name as string,
+              email: doc.data().email as string,
+              username: doc.data().username as string,
+            });
+          }
+        });
+        setLoadingGet(false);
+      }
+    })();
+  }, [params.id]);
+
+  const handleEditUser: SubmitHandler<UserFormValues> = async (data) => {
+    setLoadingEdit(true)
+    try {
+      const q = query(dataCollectionRef, where("id", "==", user.id));
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((document) => {
+        updateDoc(doc(db, "users", document.id), {
+          name: data.name,
+          email: data.email,
+          username: data.username,
+        });
+      });
+      setLoadingEdit(false)
+      toast.custom(() => (
+        <Toast variant="success" message="Successfully edit user!" />
+      ));
+      navigate("/")
+    } catch (err) {
+      setLoadingEdit(false)
+      toast.custom(() => (
+        <Toast variant="error" message="Error when edit user!" />
+      ));
+    }
   };
+
+  if (loadingGet) return null
 
   return (
     <UserManagementTemplate
@@ -29,11 +91,11 @@ function EditUser() {
       form={
         <UserForm
           defaultValue={{
-            name: searchParams.get("name") ?? "",
-            email: searchParams.get("email") ?? "",
-            username: searchParams.get("username") ?? "",
+            name: user.name,
+            email: user.email,
+            username: user.username,
           }}
-          isLoading={isLoading}
+          isLoading={loadingEdit}
           handleUser={handleEditUser}
         />
       }
